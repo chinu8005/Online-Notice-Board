5033c200f10bccece6d69eb1dead104f7c889f23
Chinmay Patil <91027505+chinu8005@users.noreply.github.com>
2022-10-26 13:09:47 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,17 @@
+<h2>Quick Contact</h2>
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	<div class="col-md-4">
+		<h2>Contact us</h2>
+		
+		Name: Chinmay Patil / Saket Nigam <br/>
+		Mobile: 8459100845<br/>
+		Email:20102184@apsit.edu.in <br/>
+		Website: <a href="https://github.com/chinu8005/">GITHUB</a>
+	</div>
+</form>	
